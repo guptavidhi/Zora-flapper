d623a1269ced9622265b1f0557b4c36e09b983d6
Vidhi Gupta <69710611+guptavidhi@users.noreply.github.com>
2023-09-29 03:23:32 -0500

    Add files via upload

---
+++ PipeSpawnerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnerScript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 2;
+    public float heightOffset = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spawn();
+
+            timer = 0;
+        }
+
+    }
+    void spawn()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint,highestPoint)), transform.rotation);
+    }
+}
